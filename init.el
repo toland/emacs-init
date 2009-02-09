952eac88d5bddfeb0fc8b4229161016340145f5f
Phillip Toland <ptoland@thehive.com>
2009-02-09 00:16:42 -0600

    Get rid of the ELPA stuff. There just aren't enough packages in ELPA to make it worthwhile.

--- init.el
+++ init.el
@@ -19,7 +19,6 @@
 (add-to-list 'load-path (concat dotfiles-dir "/vendor/jabber"))
 
 (setq autoload-file (concat dotfiles-dir "loaddefs.el"))
-(setq package-user-dir (concat dotfiles-dir "elpa"))
 (setq custom-file (concat dotfiles-dir "custom.el"))
 
 ;; These should be loaded on startup rather than autoloaded on demand
@@ -35,14 +34,6 @@
 ;; this must be loaded before ELPA since it bundles its own
 ;; out-of-date js stuff. TODO: fix it to use ELPA dependencies
 (load "vendor/nxhtml/autostart")
-
-;; Load up ELPA, the package manager
-
-(require 'package)
-(package-initialize)
-;;(require 'starter-kit-elpa)
-
-;; Load up starter kit customizations
 
 (require 'starter-kit-defuns)
 (require 'starter-kit-bindings)

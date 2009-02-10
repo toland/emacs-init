6c688d319fad7f77eb9a8e5c6072ea70fd832250
Phillip Toland <ptoland@thehive.com>
2009-02-10 10:58:48 -0600

    Aquamacs leaving its mark on my custom.el

--- custom.el
+++ custom.el
@@ -4,7 +4,15 @@
   ;; Your init file should contain only one such instance.
   ;; If there is more than one, they won't work right.
  '(cc-other-file-alist (quote (("\\.cc\\'" (".hh" ".h")) ("\\.hh\\'" (".cc" ".C")) ("\\.m\\'" (".h")) ("\\.c\\'" (".h")) ("\\.h\\'" (".c" ".cc" ".C" ".CC" ".cxx" ".cpp" ".m")) ("\\.C\\'" (".H" ".hh" ".h")) ("\\.H\\'" (".C" ".CC")) ("\\.CC\\'" (".HH" ".H" ".hh" ".h")) ("\\.HH\\'" (".CC")) ("\\.c\\+\\+\\'" (".h++" ".hh" ".h")) ("\\.h\\+\\+\\'" (".c++")) ("\\.cpp\\'" (".hpp" ".hh" ".h")) ("\\.hpp\\'" (".cpp")) ("\\.cxx\\'" (".hxx" ".hh" ".h")) ("\\.hxx\\'" (".cxx")))))
- '(javascript-indent-level 2))
+ '(javascript-indent-level 2)
+ '(aquamacs-additional-fontsets nil t)
+ '(aquamacs-autoface-mode nil)
+ '(aquamacs-customization-version-id 163 t)
+ '(current-language-environment "UTF-8")
+ '(default-frame-alist (quote ((tool-bar-lines . 0) (horizontal-scroll-bars . t) (vertical-scroll-bars . right) (cursor-type . box) (modeline . t) (fringe) (background-mode . dark) (menu-bar-lines . 1) (border-color . "black") (cursor-color . "white") (mouse-color . "Grey") (background-color . "#444444") (foreground-color . "#aaaaaa") (font . "-apple-anonymous-medium-r-normal--14-0-72-72-m-0-iso10646-1") (left-fringe) (right-fringe))))
+ '(display-time-mode nil)
+ '(toolbar-menu-show--isearch-forward t)
+ '(transient-mark-mode t))
 (custom-set-faces
   ;; custom-set-faces was added by Custom.
   ;; If you edit it by hand, you could mess it up, so be careful.

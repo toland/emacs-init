da506863b10fea08c2feaa342fdf4c12755887f6
Phillip Toland <ptoland@thehive.com>
2009-02-10 10:17:17 -0600

    Added slime configuration.

--- init-lisp.el
+++ init-lisp.el
@@ -3,6 +3,7 @@
 (define-key read-expression-map (kbd "TAB") 'lisp-complete-symbol)
 (define-key lisp-mode-shared-map (kbd "C-c l") "lambda")
 (define-key lisp-mode-shared-map (kbd "RET") 'reindent-then-newline-and-indent)
+(define-key lisp-mode-shared-map (kbd "C-j") 'newline)
 (define-key lisp-mode-shared-map (kbd "C-\\") 'lisp-complete-symbol)
 (define-key lisp-mode-shared-map (kbd "C-c v") 'eval-buffer)
 
@@ -58,6 +59,35 @@
 (add-hook 'lisp-mode-hook 'coding-hook)
 (font-lock-add-keywords 'lisp-mode
 			'(("(\\|)" . 'esk-paren-face)))
+
+;;; Slime
+
+;; Configure SLIME for OpenMCL
+(setq inferior-lisp-program "/usr/local/ccl/scripts/openmcl64")
+(setq slime-lisp-implementations
+      `((openmcl ("ccl"))
+        (sbcl ("sbcl"))))
+
+(require 'slime-autoloads)
+(add-hook 'lisp-mode-hook (lambda ()
+                            (cond ((not (featurep 'slime))
+                                   (require 'slime)
+                                   (normal-mode)))))
+
+(eval-after-load "slime"
+  '(progn
+     ;(setq load-path (append load-path (list "~/.emacs.d/lisp/slime/contrib")))
+     ;(slime-setup '(slime-fancy slime-asdf slime-banner))
+     (slime-setup)
+     (setq slime-complete-symbol*-fancy t)
+     (setq slime-complete-symbol-function 'slime-fuzzy-complete-symbol)))
+
+;; Shortcut key for starting a SLIME CL connection
+(global-set-key [f5] 'slime)
+
+;; Shortcut key for selecting SLIME buffers
+(global-set-key [(control f5)] 'slime-selector)
+
 
 (provide 'init-lisp)
 ;; init-lisp.el ends here
